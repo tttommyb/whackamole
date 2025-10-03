4175a10b0392744765a20b7f670f98cebd84b1b4
Thomas Bushnell <thomas3.bushnell@live.uwe.ac.uk>
2025-10-03 11:12:35 +0100

    Initial commit

---
+++ src/Game.cpp
@@ -0,0 +1,45 @@
+
+#include "Game.h"
+#include <iostream>
+
+Game::Game(sf::RenderWindow& game_window)
+  : window(game_window)
+{
+  srand(time(NULL));
+}
+
+Game::~Game()
+{
+
+}
+
+bool Game::init()
+{
+
+  return true;
+}
+
+void Game::update(float dt)
+{
+
+}
+
+void Game::render()
+{
+
+}
+
+void Game::mouseClicked(sf::Event event)
+{
+  //get the click position
+  sf::Vector2i click = sf::Mouse::getPosition(window);
+
+
+}
+
+void Game::keyPressed(sf::Event event)
+{
+
+}
+
+
---
+++ src/Game.h
@@ -0,0 +1,25 @@
+
+#ifndef PLATFORMER_GAME_H
+#define PLATFORMER_GAME_H
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+ public:
+  Game(sf::RenderWindow& window);
+  ~Game();
+  bool init();
+  void update(float dt);
+  void render();
+  void mouseClicked(sf::Event event);
+  void keyPressed(sf::Event event);
+
+ private:
+  sf::RenderWindow& window;
+  sf::Sprite ball;
+  sf::Texture ball_texture;
+
+};
+
+#endif // PLATFORMER_GAME_H
